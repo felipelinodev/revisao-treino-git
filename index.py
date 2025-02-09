29a61383e917cab50b6644082b56d4e5bd667957
felipelino2 <luizfelipe224434232@gmail.com>
2025-02-08 21:02:06 -0300

    melhorando-funcao-calcular
    Aqui eu adicionei uma opção na função calcular quadrado, agora além de calcular o quadrado de listas também, ordena se o usuário desejar.

--- index.py
+++ index.py
@@ -1,13 +1,21 @@
-#calcular quadrado...
-def calcular_quadrado(n = 0, lista = []):
-    if len(lista) > 0:
-        lista_quadrado = []
-        for e in lista:
-            lista_quadrado.append(e*e)
-        return lista_quadra##
-    return n*n
-
 #Algoritmo_ordenação
 def lista_ordenacao(arr):
     lista_ordenada = sorted(arr)
     return lista_ordenada
+
+
+#calcular quadrado...
+def calcular_quadrado(n=0, lista = [], order=False):
+    if len(lista) > 0:
+        lista_quadrado = []
+        for e in lista:
+            lista_quadrado.append(e*e)
+        if order == True:
+            return lista_ordenacao(lista_quadrado)
+        else:
+            return lista_quadrado
+    else:
+        return n*n
+
+
+print(calcular_quadrado(lista = [3, 33, 5, 1, 4], order=True))
